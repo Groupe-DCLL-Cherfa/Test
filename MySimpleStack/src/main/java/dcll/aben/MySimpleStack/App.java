a918a1e70c8d54e0b83540e79c505fe113af865e
Aidi othmane <XERS@XERS-PC>
2014-03-24 18:20:57 +0100

    sqd

--- MySimpleStack/src/main/java/dcll/aben/MySimpleStack/App.java
+++ MySimpleStack/src/main/java/dcll/aben/MySimpleStack/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( " World is mine !" );
+        System.out.println( " World is mine sdsqd!" );
     }
 }
